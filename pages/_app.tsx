47c6230c19e41d26f1624d22939416524c762c9c
Silvio <silvio@mailbox.org>
2020-12-26 01:59:46 +0100

    fix(axe-core-react): temporary issue
    see https://github.com/dequelabs/axe-core-npm/issues/176

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,16 @@
 // import App from "next/app";
 import { AppProps /*, AppContext */ } from 'next/app'
+import React from 'react'
 
 // Conditionally inject axe into the page.
 // This only happens outside of production and in a browser (not SSR).
 // @see: https://github.com/dequelabs/axe-core-npm/tree/develop/packages/react/examples/next.js
 // TODO: why not use next.js dynamic import?
 if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
-  const React = require('react')
   const ReactDOM = require('react-dom')
   const axe = require('@axe-core/react')
-  axe(React, ReactDOM, 1000)
+  // TODO: simplify, see https://github.com/dequelabs/axe-core-npm/issues/176
+  axe(React, ReactDOM, 1000, {}) // axe(React, ReactDOM, 1000)
 }
 
 function MyApp ({ Component, pageProps }: AppProps) {
