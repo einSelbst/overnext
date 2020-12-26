ab5df8d508dd46d22537485f3bdd677702c72fdf
Silvio <silvio@mailbox.org>
2020-12-26 01:44:00 +0100

    feat(a11y): load axe-core-react in non-production mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 // import App from "next/app";
 import { AppProps /*, AppContext */ } from 'next/app'
 
+// Conditionally inject axe into the page.
+// This only happens outside of production and in a browser (not SSR).
+// @see: https://github.com/dequelabs/axe-core-npm/tree/develop/packages/react/examples/next.js
+// TODO: why not use next.js dynamic import?
+if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
+  const React = require('react')
+  const ReactDOM = require('react-dom')
+  const axe = require('@axe-core/react')
+  axe(React, ReactDOM, 1000)
+}
+
 function MyApp ({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
