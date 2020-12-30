a2d8aff6983f4402865229c065dbc968d796d1e6
Silvio <silvio@mailbox.org>
2020-12-30 03:13:18 +0100

    test(coverage): add config to measure code coverage
    see https://github.com/bahmutov/next-and-cypress-example

---
+++ src/pages/api/__coverage__.ts
@@ -0,0 +1,13 @@
+// Report API code coverage: https://github.com/bahmutov/next-and-cypress-example
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const coverage = (req: NextApiRequest, res: NextApiResponse) => {
+  res.statusCode = 200
+  res.json({
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore TS2339
+    coverage: global.__coverage__ || null
+  })
+}
+
+export default coverage
