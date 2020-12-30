d17328cc44a628458aa58d4bd2431d9d7c25ca9b
Silvio <silvio@mailbox.org>
2020-12-30 02:43:17 +0100

    test: fix broken specs

--- cypress/integration/examples/querying.spec.js
+++ cypress/integration/examples/querying.spec.js
@@ -19,23 +19,23 @@ context('Querying', () => {
     //              ↲
     // Use CSS selectors just like jQuery
 
-    cy.get('[data-it-id="it-example"]').should('have.class', 'example')
+    cy.get('[data-test-id="test-example"]').should('have.class', 'example')
 
     // 'cy.get()' yields jQuery object, you can get its attribute
     // by invoking `.attr()` method
-    cy.get('[data-it-id="it-example"]')
-      .invoke('attr', 'data-it-id')
-      .should('equal', 'it-example')
+    cy.get('[data-test-id="test-example"]')
+      .invoke('attr', 'data-test-id')
+      .should('equal', 'test-example')
 
     // or you can get element's CSS property
-    cy.get('[data-it-id="it-example"]')
+    cy.get('[data-test-id="test-example"]')
       .invoke('css', 'position')
       .should('equal', 'static')
 
     // or use assertions directly during 'cy.get()'
     // https://on.cypress.io/assertions
-    cy.get('[data-it-id="it-example"]')
-      .should('have.attr', 'data-it-id', 'it-example')
+    cy.get('[data-test-id="test-example"]')
+      .should('have.attr', 'data-test-id', 'test-example')
       .and('have.css', 'position', 'static')
   })
 
