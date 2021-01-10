5d9f76cbff3a41b5485238991980e3c9fb626bc9
Silvio <silvio@mailbox.org>
2021-01-10 03:15:36 +0100

    style: more strict import ordering

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@
  * @param [plugin: function, configuration?: object, phases?: array]
  * @see: https://github.com/cyrilwanner/next-compose-plugins
  */
-const { withPlugins, optional } = require('next-compose-plugins')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
+const { withPlugins, optional } = require('next-compose-plugins')
 
 const nextConfiguration = {
   i18n: {
