b859c1c812f301d9d4bcdab40a3e307c6e33bcfe
Silvio <silvio@mailbox.org>
2021-02-14 01:03:41 +0100

    refactor: getStaticPaths for localized dynamic routes

--- src/pages/[dynamic]/index.tsx
+++ src/pages/[dynamic]/index.tsx
@@ -41,39 +41,35 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
   }
 }
 
+/**
+ * see: https://github.com/vercel/commerce/blob/master/pages/%5B...pages%5D.tsx
+ * combine paths with locales so that all pages can be pre-rendered
+ */
 export const getStaticPaths: GetStaticPaths = async ({ locales }) => {
-  await new Promise(resolve => resolve('Hello'))
-  const rawPaths = Array.from({ length: 10 }, (_, index) => ({
+  await new Promise(resolve => resolve('async needs await'))
+  // the paths would come from an api, but to have something to work with:
+  let paths = Array.from({ length: 10 }, (_, index) => ({
     params: {
       dynamic: `page-${index}`,
     },
-    url: `page-${index}`,
   }))
 
-  const pathss = rawPaths
-    .map(page => page.url)
-    .filter(url => {
-      /* console.log(url) */
-      /* console.log(locales) */
-      return url
-      //if (!url || !locales) return url
-      /* console.log('shouldnt log') */
-      // If there are locales, only include the pages that include one of the available locales
-      //if (locales.includes(url.split('/')[0])) return url
-
-      //invalidPaths.push(url)
+  if (locales) {
+    const pathsI18n = paths.map(path => {
+      return locales.map(locale => {
+        return { params: path.params, locale: locale }
+      })
     })
-  /* console.log(paths) */
-  /* console.log("something") */
+    paths = pathsI18n.reduce((a, v) => a.concat(v), paths)
+  }
+
   return {
-    /* export async function agetStaticPaths({ locales }: GetStaticPathsContext) { */
-    //const { pages } = await getAllPages()
-    //const [invalidPaths, log] = missingLocaleInPages()
-    paths: [
-      { params: { slug: 'page-1', dynamic: 'page-1', pathss } },
-      { params: { slug: 'page-1', dynamic: 'page-1' }, locale: 'en' },
-      { params: { slug: 'page-1', dynamic: 'page-1' }, locale: 'fr' },
-    ],
+    paths: paths,
+    /* paths: [
+     *   { params: { dynamic: 'page-1' } },
+     *   { params: { dynamic: 'page-1' }, locale: 'en' },
+     *   { params: { dynamic: 'page-1' }, locale: 'fr' },
+     * ], */
     // Fallback shouldn't be enabled here or otherwise this route
     // will catch every page, even 404s, and we don't want that
     fallback: false,
