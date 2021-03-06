dcf570a063a14244263c6b0b3892eae5ecbb5ec9
Silvio <silvio@mailbox.org>
2021-03-06 21:25:58 +0100

    feat: add env config module

---
+++ src/config/env.config.ts
@@ -0,0 +1,7 @@
+const ENVIRONMENT = {
+  PRODUCTION: process.env.NODE_ENV === 'production',
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_Coalescing_Operator
+  SITE_URL: process.env.SITE_URL ?? 'https://overnext.vercel.app',
+}
+
+export { ENVIRONMENT }
