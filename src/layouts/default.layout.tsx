1546413cdb221c414db961708ee9a35524b8196f
Silvio <silvio@mailbox.org>
2021-04-04 22:22:47 +0200

    style: remove inline css from default layout

--- src/layouts/default.layout.tsx
+++ src/layouts/default.layout.tsx
@@ -7,52 +7,33 @@ const DefaultLayout = ({
 }): JSX.Element => {
   return (
     <>
-      <header style={{ background: 'lightblue' }}>
+      <header>
         <nav>
           <Link href='/'>
             <a>Home</a>
           </Link>{' '}
-          |
-          <Link href='/about'>
-            <a href='legal/about'>About</a>
+          |{' '}
+          <Link href='/legal/about'>
+            <a>About</a>
           </Link>{' '}
-          |
-          <Link href='/contact'>
-            <a href='legal/contact'>Contact</a>
+          |{' '}
+          <Link href='/legal/privacy'>
+            <a>Privacy</a>
+          </Link>{' '}
+          |{' '}
+          <Link href='/legal/contact'>
+            <a>Contact</a>
           </Link>
         </nav>
       </header>
 
-      <main style={{ border: '4px dashed green' }}>
-        <header>
-          <h1>Page Headline</h1>
-        </header>
+      {children}
 
-        <section aria-label='quick summary'>
-          Summary Text. Visit this for more info:
-          https://www.smashingmagazine.com/2020/01/html5-article-section/
-        </section>
-
-        <article>
-          <header>
-            <p>The Header of the article</p>
-          </header>
-          <section className='introduction'></section>
-          <section className='content'>{children}</section>
-          <section className='summary'></section>
-          <footer>
-            <p>The footer of the article</p>
-          </footer>
-        </article>
-      </main>
-
-      <aside
-        style={{ background: 'darkslateblue', color: 'lightgoldenrodyellow' }}
-      >
+      <aside>
         <p>Something which is supposed to be on the side</p>
       </aside>
 
-      <footer style={{ background: 'turquoise' }}>
+      <footer>
         <p>©Copyright 2050 by nobody. All rights reversed.</p>
       </footer>
     </>
