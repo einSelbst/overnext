f25d6f47d7b95e427fa2cc7f6aa9679f709d304a
Silvio <silvio@mailbox.org>
2021-04-07 20:42:07 +0200

    fix: a few docstrings

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -3,10 +3,17 @@ export default {
 
   /**
    * Determines if we are running on server or in the client.
-   * @return {boolean} true if running on server
+   *
+   * @returns `true` if running on server, `false` if in browser context
    */
   SERVER_RENDERED: typeof window === 'undefined',
 
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_Coalescing_Operator
+  /**
+   * Used in the sitemap to generate the URLs
+   *
+   * @remarks
+   * Regarding the `??`:
+   * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_Coalescing_Operator | Nullish Coalescing Operator}
+   */
   SITE_URL: process.env.SITE_URL ?? 'https://overnext.vercel.app',
 }
