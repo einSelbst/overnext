ff6746f05fefb72b488df7b91436e80d611c9eee
Silvio <silvio@mailbox.org>
2021-04-09 00:33:05 +0200

    Revert "style: write component for apple-icon meta tag"
    This reverts commit 1745c6eb89af5430682bd68eca966dad1af2f383.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -66,28 +66,6 @@ if (!ENV.SERVER_RENDERED && !ENV.PRODUCTION) {
   axe(React, ReactDOM, 1000, {})
 }
 
-const IconLink = ({
-  rel = 'icon',
-  size,
-  href,
-}: {
-  rel: string
-  size: string
-  href: string
-}): JSX.Element => {
-  return <link rel={rel} sizes={`${size}x${size}`} href={href} />
-}
-
-const IconLinkApple = ({ size }: { size: string }): JSX.Element => {
-  return (
-    <IconLink
-      rel='apple-touch-icon'
-      size={size}
-      href={`/icons/apple-touch-icon-${size}x${size}.png`}
-    />
-  )
-}
-
 const HeadIcons = (): JSX.Element => {
   const isNotAmp = !useAmp()
 
@@ -132,13 +110,41 @@ const HeadIcons = (): JSX.Element => {
         sizes='16x16'
         href='/icons/icon-16x16.png'
       />
-      <IconLinkApple size='57' />
-      <IconLinkApple size='72' />
-      <IconLinkApple size='76' />
-      <IconLinkApple size='114' />
-      <IconLinkApple size='120' />
-      <IconLinkApple size='144' />
-      <IconLinkApple size='152' />
+      <link
+        rel='apple-touch-icon'
+        sizes='57x57'
+        href='/icons/apple-touch-icon-57x57.png'
+      />
+      <link
+        rel='apple-touch-icon'
+        sizes='72x72'
+        href='/icons/apple-touch-icon-72x72.png'
+      />
+      <link
+        rel='apple-touch-icon'
+        sizes='76x76'
+        href='/icons/apple-touch-icon-76x76.png'
+      />
+      <link
+        rel='apple-touch-icon'
+        sizes='114x114'
+        href='/icons/apple-touch-icon-114x114.png'
+      />
+      <link
+        rel='apple-touch-icon'
+        sizes='120x120'
+        href='/icons/apple-touch-icon-120x120.png'
+      />
+      <link
+        rel='apple-touch-icon'
+        sizes='144x144'
+        href='/icons/apple-touch-icon-144x144.png'
+      />
+      <link
+        rel='apple-touch-icon'
+        sizes='152x152'
+        href='/icons/apple-touch-icon-152x152.png'
+      />
       <link
         rel='apple-touch-icon'
         sizes='180x180'
