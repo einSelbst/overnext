c82673150ad25c0f37a7ffd5144626abc808b605
einSelbst <silvio@mailbox.org>
2021-04-16 23:58:19 +0200

    build(babel): change babel config file format, add istanbul back in

---
+++ babel.config.js
@@ -0,0 +1,29 @@
+const presets = [
+  [
+    'next/babel',
+    {
+      '@babel/preset-env': {
+        targets: 'defaults',
+        useBuiltIns: 'entry',
+        // corejs version must be kept in sync with package.json
+        // maybe automate with https://docs.renovatebot.com/configuration-options/#postupdateoptions
+        corejs: '3.10.1',
+      },
+    },
+  ],
+]
+const plugins = [
+  /* ["istanbul"] */
+]
+
+if (process.env.APP_ENV === 'test') {
+  // only load test coverage instrumentation when needed
+  // eslint-disable-next-line no-console
+  console.log('Loading coverage instrumentation')
+  plugins.push('istanbul')
+}
+
+module.exports = { presets, plugins }
+/* module.exports = function(api) {
+ *   return {presets, plugins};
+ * }; */
