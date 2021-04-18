455026425969befae4b1238d7093b66acb8ed0d8
Silvio <silvio@mailbox.org>
2021-04-18 16:52:48 +0200

    chore: remove commented out code
    I don't want to go SSR only for sure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -183,17 +183,5 @@ function _app ({ Component, pageProps }: AppLayoutProps): JSX.Element {
   )
 }
 
-// Only uncomment this method if you have blocking data requirements for
-// every single page in your application. This disables the ability to
-// perform automatic static optimization, causing every page in your app to
-// be server-side rendered.
-//
-// _app.getInitialProps = async (appContext: AppContext) => {
-//   // calls page's `getInitialProps` and fills `appProps.pageProps`
-//   const appProps = await App.getInitialProps(appContext);
-
-//   return { ...appProps }
-// }
-
 export { reportWebVitals }
 export default _app
