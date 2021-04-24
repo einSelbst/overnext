43ab66bd09192d744f4ee9da7830671126e804a3
einSelbst <silvio@mailbox.org>
2021-04-24 21:51:48 +0200

    feat: add example image in hybrid page

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,20 @@
-// Declarations for modules without types
-// not sure what this is good for but saw this in other next.js apps which use next-themes
-declare module 'next-themes'
+/**
+ * Declarations for modules without types
+ *
+ * This file is a global script, that means it shouldn't have any import/export statement
+ * @see {@link https://stackoverflow.com/a/42257742/531439}
+ *
+ */
+// Type errors when I declare this...
+// declare module 'next-themes'
+
+// for amp, which doesn't have type definitions
+// keep it simple and use any
+// @see: {@link https://stackoverflow.com/a/50601125/531439}
+declare namespace JSX {
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  interface IntrinsicElements {
+    'amp-img': any
+  }
+  /* eslint-enable @typescript-eslint/no-explicit-any */
+}
