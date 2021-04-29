ba04969c7bb66051d78001b660bf83d4ccda474c
einSelbst <silvio@mailbox.org>
2021-04-29 18:49:25 +0200

    style: minor linter import config change
    not sure but better try and see

--- test/test-utils.ts
+++ test/test-utils.ts
@@ -18,7 +18,9 @@ const customRender = (ui, options = {}) =>
   render(ui, { wrapper: Providers, ...options })
 
 // re-export everything
+// eslint-disable-next-line import/export
 export * from '@testing-library/react'
 
 // override render method
+// eslint-disable-next-line import/export
 export { customRender as render }
