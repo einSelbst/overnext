fe1e96afb141b829138bd504efb2ad33fb987bb6
einSelbst <silvio@mailbox.org>
2021-04-30 12:59:10 +0200

    refactor: move custom typings into folder

---
+++ src/typings/layout.d.ts
@@ -0,0 +1,31 @@
+// see: https://github.com/arasfeld/next-graphql-app/blob/main/next-env.d.ts
+import type {
+  NextComponentType,
+  NextPageContext,
+  NextLayoutComponentType,
+} from 'next'
+import type { AppProps } from 'next/app'
+
+declare module 'next' {
+  type NextLayoutComponentType<P = Record<string, unknown>> = NextComponentType<
+    NextPageContext,
+    unknown,
+    P
+  > & {
+    Layout?: ReactNode
+  }
+
+  type NextLayoutPage<P = Record<string, unknown>, IP = P> = NextComponentType<
+    NextPageContext,
+    IP,
+    P
+  > & {
+    Layout: ReactNode
+  }
+}
+
+declare module 'next/app' {
+  type AppLayoutProps<P = unknown> = AppProps<P> & {
+    Component: NextLayoutComponentType
+  }
+}
