3440e051cd8e908b7468f644c051e3ecabb25d48
einSelbst <silvio@mailbox.org>
2021-05-03 00:53:41 +0200

    fix(a11y): add title to 2nd search box

--- src/layouts/site.layout.js
+++ src/layouts/site.layout.js
@@ -24,6 +24,7 @@ const SiteLayout = ({ children }) => (
             </div>
             <div className='mt-2'>
               <input
+                title='Search again'
                 className='block w-full border border-gray-300 rounded-lg bg-gray-100 px-3 py-2 leading-tight focus:outline-none focus:border-gray-600 focus:bg-white'
                 placeholder='Search...'
               />
