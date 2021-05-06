3ff2252cbd832236a5fc4ceb4ce45d7caeb7b2bd
einSelbst <silvio@mailbox.org>
2021-05-06 15:22:58 +0200

    build: type is missing on ci

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,11 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 declare module '@moxy/next-layout'
+
+type ComponentReturnType =
+  | ReactElement
+  | Array<ComponentReturnType>
+  | string
+  | number
+  | boolean
+  | null // Note: undefined is invalid
