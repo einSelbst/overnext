01217400db1d56ce73f7f6f1bee0ba8e75604016
einSelbst <silvio@mailbox.org>
2021-05-07 14:42:24 +0200

    wip: use a custom type for components

--- src/pages/hybrid/one.tsx
+++ src/pages/hybrid/one.tsx
@@ -1,3 +1,4 @@
+import { OverNextComponentType } from 'next'
 import { useAmp } from 'next/amp'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
@@ -9,9 +10,19 @@ export const config = {
 }
 
 /**
+ * Using 'normal' props instead of prop-types package
+ * @see navigation.tsx for an example with prop-types
+ */
+type Props = {
+  success: boolean
+  error: boolean
+}
+
+/**
  * HTML entities should be escaped
  */
-const One = (): JSX.Element => {
+/* const One = (props: Props): JSX.Element => { */
+const One = (props: Props): OverNextComponentType => {
   useEffect(() => {
     console.log('useEffect default')
 
@@ -54,6 +65,22 @@ const One = (): JSX.Element => {
         I&apos;m a hybrid page and I&apos;m available in multiple languages!
       </p>
       {isAmp && <p>Now I am AMP! (escaping entity is difficult here)</p>}
+
+      {/* @see https://github.com/sindresorhus/react-extras#choose */}
+      <div className='h-8 w-8'>
+        {(() => {
+          if (props.success) {
+            return <span>{props.success}</span>
+          }
+
+          if (props.error) {
+            return <span>{props.error}</span>
+          }
+
+          return <span>�</span>
+        })()}
+      </div>
+
       <p>
         {isAmp ? (
           <Link href='/hybrid/one'>
