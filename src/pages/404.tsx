33ae66cf347a813270075b8b6cc80ac18eb95f25
einSelbst <silvio@mailbox.org>
2021-05-12 15:45:26 +0200

    ux: add nicer error message

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,13 @@
 const Custom404 = (): JSX.Element => {
-  return <h1>404 - Page Not Found</h1>
+  return (
+    <>
+      <h1>404 - Page Not Found</h1>
+      <p>
+        OMG what happened? Yesterday everything was working fine and now
+        it&apos;s broken!?
+      </p>
+    </>
+  )
 }
 
 export default Custom404
