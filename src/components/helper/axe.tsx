4b1065162ea58817ad501e3e0899a53a828cc81e
Silvio <silvio@mailbox.org>
2021-05-25 01:18:50 +0200

    test(a11y): cleanup axe helper

--- src/components/helper/axe.tsx
+++ src/components/helper/axe.tsx
@@ -1,19 +1,30 @@
-/* import axe from '@axe-core/react' */
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-/* const Axe = (): JSX.Element => { */
-const Axe = (): null => {
-  /* void axe(React, ReactDOM, 1000, {}) */
+/**
+ * Configuration for Axe. Unfortunately I have no clue how the default config looks.
+ *
+ * @see {@link https://github.com/dequelabs/axe-core/blob/master/doc/API.md#api-name-axeconfigure}
+ */
+const axeConfig = {
+  /*   rules: [
+    { id: 'heading-order', enabled: true },
+    { id: 'label-title-only', enabled: true },
+    { id: 'link-in-text-block', enabled: true },
+    { id: 'region', enabled: true },
+    { id: 'skip-link', enabled: true },
+    { id: 'help-same-as-label', enabled: true },
+  ],
+ */
+}
 
+const Axe = (): null => {
   if (process.env.NODE_ENV === 'development') {
     void (async () => {
       try {
         const { default: axe } = await import('@axe-core/react')
-        void axe(React, ReactDOM, 1000, {})
-        console.log('axe is ready')
+        await axe(React, ReactDOM, 1000, axeConfig)
       } catch {
-        // console.log(err);
         console.log('axe not ready yet')
       }
     })()
