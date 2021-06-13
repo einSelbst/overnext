3944a87c44d55a5231037f7344b31215b86664ee
einSelbst <silvio@mailbox.org>
2021-06-14 00:00:49 +0200

    dx: add condition to todo

--- src/components/layout/navigation.tsx
+++ src/components/layout/navigation.tsx
@@ -48,7 +48,7 @@ const Navigation = (props: Props): ComponentReturnType => (
     <div className='mt-2'>
       <label htmlFor='search-global'>
         <span>Discover Vehicles</span>
-        {/* ToDo: localize title */}
+        {/* ToDo [+next-i18next]: localize title */}
         <input
           className='block w-full border border-gray-300 rounded-lg bg-gray-100 px-3 py-2 leading-tight focus:outline-none focus:border-gray-600 focus:bg-white'
           id='search-global'
