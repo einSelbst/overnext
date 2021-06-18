97f559130520d2c6bec586b4a02b153edf5f1b6c
einSelbst <silvio@mailbox.org>
2021-06-18 11:50:07 +0200

    wip: center some logos, still experimenting here

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -8,22 +8,23 @@ const NOTIFICATION_STATES: Record<string, string> = {
 
 const Vercel = () => (
   <a href='https://vercel.com' rel='noopener noreferrer' target='_blank'>
-    Powered by{' '}
     <img
-      alt='Vercel Logo'
+      alt='Visit vercel.com'
       className='logo'
-      height='16px'
-      src='/vercel.svg'
-      width='71px'
+      height='24px'
+      src='/logos/vercel-logo.svg'
+      width='106px'
+      style={{ margin: '10px auto' }}
     />
   </a>
 )
 
 const Netlify = () => (
   <>
-    <a href='https://www.netlify.com'>
+    <a href='https://www.netlify.com' rel='noopener noreferrer' target='_blank'>
       <img
-        alt='Deploys by Netlify'
+        alt='visit netlify.com'
+        className='logo'
         height='51'
         src='https://www.netlify.com/img/global/badges/netlify-color-accent.svg'
         width='114'
@@ -49,12 +50,20 @@ const Netlify = () => (
 )
 
 const Amplify = () => (
-  <a
-    href='https://main.d2y25he53ruhoy.amplifyapp.com'
-    rel='noopener noreferrer'
-    target='_blank'>
-    Amplify
-  </a>
+  <center>
+    <a
+      href='https://main.d2y25he53ruhoy.amplifyapp.com'
+      rel='noopener noreferrer'
+      target='_blank'>
+      <img
+        alt='visit AWS Amplify'
+        className='logo'
+        height='24px'
+        src='/logos/aws-amplify-logo.png'
+        width='169px'
+      />
+    </a>
+  </center>
 )
 
 const HOSTER: Record<string, React.ReactNode> = {
@@ -71,17 +80,10 @@ const Footer = ({
   platform?: keyof typeof HOSTER
   notification?: keyof typeof NOTIFICATION_STATES
 }): ComponentReturnType => {
-  console.log(HOSTER[platform])
-  console.log(notification)
-  console.log(platform)
-  console.log(HOSTER.NETLIFY)
-
   return (
     <footer>
-      <div>{NOTIFICATION_STATES[notification]}</div>
-      <div>{HOSTER[ENV.PLATFORMX]}</div>
-      <div>{HOSTER.VERCEL}</div>
-      <p>Hosted on {process.env.platform}</p>
+      <p>Hosted on</p>
+      {HOSTER[ENV.PLATFORMX]}
       <p>©Copyright 2050 by nobody. All rights reversed.</p>
     </footer>
   )
