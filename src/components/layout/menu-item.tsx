b054258025c8cfab9a0206425bb5d46bafee5566
einSelbst <silvio@mailbox.org>
2021-07-03 00:16:43 +0200

    fix: linter issues

--- src/components/layout/menu-item.tsx
+++ src/components/layout/menu-item.tsx
@@ -1,50 +1,28 @@
 import Link from 'next/link'
-import * as React from 'react'
 /* import { FaCarrot } from 'react-icons/fa' */
 
-/**
- * @see {@link https://gist.github.com/busypeoples/6ac09e8dd63a12f78603fd76aa9cf580}
- * @see {@link https://https://dev.to/busypeoples/notes-on-typescript-inferring-react-proptypes-1g88}
- */
-type InferPropertyTypes<
-  PropertyTypes,
-  DefaultProps = unknown,
-  Props = PropertyTypes
-> = {
-  [Key in keyof Props]: Key extends keyof DefaultProps
-    ? DefaultProps[Key] | Props[Key]
-    : Props[Key]
+type MenuItemPropertyType = {
+  path: string
+  title: string
 }
 
 /**
  * A single Menu Item for usage in navigation
  */
-const MenuItem: MenuItemProps = (props: MenuItemProps): React.ReactElement => (
+const MenuItem = ({
+  path,
+  title,
+}: MenuItemPropertyType): React.ReactElement => (
   /* const myIcon = React.createElement(props.icon) */
   <>
-    <Link href={props.path}>
+    <Link href={path}>
       <a>
         {/* {myIcon} */}
-        {props.title}
+        {title}
       </a>
     </Link>
     &nbsp; &nbsp; &nbsp;
   </>
 )
-
-type menuItemPropertyTypes = {
-  path: String
-  title: String
-}
-;/ * icon: PropTypes.func.isRequired, * /
-
-const menuItemDefaultProps = {
-  /* icon: FaCarrot, */
-}
-
-type MenuItemProps = InferPropertyTypes<
-  menuItemPropertyTypes,
-  typeof menuItemDefaultProps
->
 
 export default MenuItem
