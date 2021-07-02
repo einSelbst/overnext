05700113ccc5f45cd4c0b77779022c1c6fd3a6c5
einSelbst <silvio@mailbox.org>
2021-07-03 00:48:43 +0200

    ui: add more items to the menu

--- src/components/layout/navigation.tsx
+++ src/components/layout/navigation.tsx
@@ -13,6 +13,11 @@ const Navigation = (props: Props): ComponentReturnType => (
       <MenuItem path='/' title='Home' />
       <MenuItem path='/dog' title='Dog' />
       <MenuItem path='/page-1' title='Page 1' />
+      <MenuItem path='/first-amp' title='First AMP' />
+      <MenuItem path='/hybrid/one' title='One' />
+      <MenuItem path='/hybrid/post/1' title='Post 1' />
+      <MenuItem path='/hybrid/post/4' title='Post 4' />
+      <MenuItem path='/hybrid/foo/bar' title='Foo Bar' />
       <MenuItem path='/legal/about' title='About' />
       <MenuItem path='/legal/privacy' title='Privacy' />
       <MenuItem path='/legal/contact' title='Contact' />
