c9a7963ef17c07e78d46d8bff2007d2610926554
einSelbst <silvio@mailbox.org>
2022-02-08 17:43:15 +0100

    test: jest config changes
    this uses babel again, but I have used ts-jest in between and nextjs now would use swc

--- jest.config.js
+++ jest.config.js
@@ -8,40 +8,59 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
-  moduleDirectories: ['node_modules', '<rootDir>/'],
-
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jest-environment-jsdom',
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(test).[jt]s?(x)'],
-}
-
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)
-
-const _oldmoduleexports = {
   globals: {
     'ts-jest': {
       isolatedModules: true,
       useESM: true,
     },
   },
+  preset: 'ts-jest/presets/default-esm',
+  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
+
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
   },
-  preset: 'ts-jest/presets/default-esm',
-  roots: ['<rootDir>'],
-  testEnvironment: 'jsdom',
+
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(test).[jt]s?(x)'],
   testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
+
   transform: {
-    '^.+\\.(ts|tsx)$': 'babel-jest',
+    // the first line would mean swc is used in tests, but it gives a warning I don't know how to get rid of
+    /* '^.+\\.(ts|tsx)$': 'babel-jest', */
+    // so I'm optiong out of swc for jest for now
+    '^.+\\.(js|jsx|ts|tsx)$': [
+      'babel-jest',
+      {
+        presets: [
+          [
+            'next/babel',
+            {
+              'styled-jsx': {
+                'babel-test': true,
+              },
+            },
+          ],
+        ],
+      },
+    ],
   },
-  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
+  /* transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'], */
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+
   watchPlugins: [
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname',
   ],
 }
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig)
