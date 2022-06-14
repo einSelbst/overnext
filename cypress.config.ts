a5ef04e1af9219944a12e447436e3560f85b7141
einSelbst <silvio@mailbox.org>
2022-06-14 23:25:15 +0200

    test: add cypress component testing basics

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,28 +2,43 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   chromeWebSecurity: true,
+
+  component: {
+    devServer: {
+      bundler: 'webpack',
+      framework: 'next',
+    },
+  },
+
   'cypress-watch-and-reload': {
     watch: ['src/**/*'],
   },
+
+  /*
+   * We've imported your old cypress plugins here.
+   * You may want to clean this up later by importing these.
+   */
+  e2e: {
+    baseUrl: 'http://localhost:3004',
+    excludeSpecPattern: '**/examples/*.spec.js',
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins')(on, config)
+    },
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+
   env: {
     codeCoverage: {
       url: '/api/__coverage__',
     },
   },
+
   projectId: 'esdwok',
+
   retries: {
-    runMode: 2,
     openMode: 0,
+    runMode: 2,
   },
+
   videoUploadOnPasses: false,
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
-    baseUrl: 'http://localhost:3004',
-    excludeSpecPattern: '**/examples/*.spec.js',
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
-  },
 })
